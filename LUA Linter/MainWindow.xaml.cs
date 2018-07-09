7afe4df855af982096678ea3cd0d7b9399dc1d2e
malgrimace <36894563+malgrimace@users.noreply.github.com>
2018-07-09 15:30:45 +0100

    Updated list clearance
    Error list now removing errors which have been corrected in display listbox.
    
    Separated clearing Display and Error lists to allow methods to execute at different times.
    
    Both lists now clearing correctly when loading a new file.
    
    Items in Error list marked as Readonly.

--- LUA Linter/MainWindow.xaml.cs
+++ LUA Linter/MainWindow.xaml.cs
@@ -3,9 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using System.Windows;
-using System.Windows.Input;
 
 namespace LUA_Linter
 {
@@ -16,8 +14,7 @@ namespace LUA_Linter
 
         private int lineNumber;
         public FileInfo LuaFile;
-        private string line;
-        private string error;
+        private string _line;
 
 
         public MainWindow()
@@ -30,14 +27,45 @@ namespace LUA_Linter
 
         private void LoadFile(object sender, RoutedEventArgs e)
         {
-            ClearList();
-            lineNumber = 0;
-
-            OpenFileDialog openFile = new OpenFileDialog();
-            openFile.Filter = "Lua files (*.lua)|*.lua";
+            OpenFileDialog openFile = new OpenFileDialog
+            {
+                Filter = "Lua files (*.lua)|*.lua"
+            };
 
             Nullable<bool> result = openFile.ShowDialog();
 
+
+            //if valid file is selected, open file, repeatedly add line to datacontext  until the end
+            if (result == true)
+            {
+                ClearDisplayList();
+                clearErrorList();
+                lineNumber = 0;
+
+                StreamReader fileContents = new StreamReader(openFile.FileName);
+                
+                while ((_line = fileContents.ReadLine()) != null)
+                {
+                    lineNumber++; // adds line numbers to listbox items
+
+                    // display contents of Lua file in listbox
+                    MainWindowDataContext.luaFileInfo.Add(new luaFileContents
+                    {
+                        body = lineNumber + "   " + _line
+                    });
+                }
+
+                checkForErrors();
+            }
+            else
+            {
+                MessageBox.Show("Please select a file");
+            }
+        }
+
+
+        private void checkForErrors()
+        {
             Dictionary<string, string> commonMistakes = new Dictionary<string, string>
             {
                 {"say(", "SAY(" },
@@ -46,56 +74,66 @@ namespace LUA_Linter
                 {"answer(", "ANSWER(" }
             };
 
-            //if valid file is selected, open file, repeatedly add line to datacontext  until the end
-            if (result == true)
+            foreach (var line in MainWindowDataContext.luaFileInfo)
             {
-                StreamReader fileContents = new StreamReader(openFile.FileName);
-                
-                while ((line = fileContents.ReadLine()) != null)
-                {
-                    lineNumber++; // adds line numbers to listbox items
+                var value = line.body;
 
-                    // if in error dictionary, add to error list
-                    foreach (var keyValuePair in commonMistakes)
+                // if in error dictionary, add to error list
+                foreach (var keyValuePair in commonMistakes)
+                {
+                    if (value.Contains(keyValuePair.Key))
                     {
-                        if (line.Contains(keyValuePair.Key))
+                        var dicKey = keyValuePair.Key;
+                        var dicValue = keyValuePair.Value;
+
+                        MainWindowDataContext.errorList.Add(new luaFileErrors
                         {
-                            var dicKey = keyValuePair.Key;
-                            var dicValue = keyValuePair.Value;
-                            MainWindowDataContext.errorList.Add(new luaFileErrors
-                            {
-                                contentError = lineNumber + " " + line  + "\r" + "Should '"  + dicKey + "' be '" + dicValue + "'?" + "\r"
-                            });
-                        }
+                            contentError = value + "\r" + "Should '" + dicKey + "' be '" + dicValue + "'?" + "\r"
+                        });
                     }
-                    
-                    // display contents of Lua file in listbox
-                    MainWindowDataContext.luaFileInfo.Add(new luaFileContents
-                    {
-                        fileName = openFile.FileName,
-                        body = lineNumber + "   " + line
-                    });
                 }
-            }
-            else
-            {
-                MessageBox.Show("Please select a file");
             }
         }
 
- 
-        private void ClearList()
+
+        private void scanFile(object sender, RoutedEventArgs e)
+        {
+            clearErrorList();
+            checkForErrors();
+        }
+
+
+        private void ClearDisplayList()
         {
             ObservableCollection<luaFileContents> itemsToRemove = new ObservableCollection<luaFileContents>();
 
             foreach (luaFileContents item in lb_display.Items)
             {
+                //Console.WriteLine("Removing" + item.body + "from display list...");
                 itemsToRemove.Add(item);
             }
 
             foreach (luaFileContents item in itemsToRemove)
             {
+                //Console.WriteLine("Removed " + item.body + "from display list...");
                 ((ObservableCollection<luaFileContents>)lb_display.ItemsSource).Remove(item);
+            }
+        }
+
+        private void clearErrorList()
+        {
+            ObservableCollection<luaFileErrors> errorsToRemove = new ObservableCollection<luaFileErrors>();
+
+            foreach (luaFileErrors item in lb_errorDisplay.Items)
+            {
+                //Console.WriteLine("Removing" + item.contentError + "from error list...");
+                errorsToRemove.Add(item);
+            }
+
+            foreach (luaFileErrors item in errorsToRemove)
+            {
+                //Console.WriteLine("Removed " + item.contentError + "from error list...");
+                ((ObservableCollection<luaFileErrors>) lb_errorDisplay.ItemsSource).Remove(item);
             }
         }
     }
